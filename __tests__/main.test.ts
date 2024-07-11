b7ddbd0d18c21a33f78e8680ce30d04e4d7d54fe
Paul Ebose <paulebose@gmail.com>
2024-07-11 17:51:54 +0100

    add tests for unsupported events and authors

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,11 +6,48 @@
  * variables following the pattern `INPUT_<INPUT_NAME>`.
  */
 
+import * as core from '@actions/core'
 import * as main from '../src/main'
+import * as actionInputsUtils from '../src/utils/action-inputs'
+import * as helperUtils from '../src/utils/helpers'
+
+// Spy on the action's main function
+const runSpy = jest.spyOn(main, 'run')
+
+// Spy on the GitHub Actions core library
+const getInputSpy = jest.spyOn(core, 'getInput')
+const setOutputSpy = jest.spyOn(core, 'setOutput')
+
+// Spy on the acton's utils
+const getActionInputsSpyMock = jest.spyOn(actionInputsUtils, 'getActionInputs')
+const isFirstTimeContributorSpy = jest.spyOn(helperUtils, 'isFirstTimeContributor')
+const isSupportedEventSpy = jest.spyOn(helperUtils, 'isSupportedEvent')
 
 describe('first-contribution GitHub Action', () => {
-  // Check if unsupported event
-  // Check if octokit is call with correct token
+  it('exit action when the triggered event is NOT supported', async () => {
+    isSupportedEventSpy.mockReturnValue(false)
+
+    await main.run()
+
+    expect(isSupportedEventSpy).toHaveBeenCalled()
+    expect(isFirstTimeContributorSpy).not.toHaveBeenCalled()
+    expect(getActionInputsSpyMock).not.toHaveBeenCalled()
+    expect(setOutputSpy).not.toHaveBeenCalled()
+    expect(runSpy).toHaveReturned()
+  })
+
+  it('exit action when the issue or pull request author is NOT a first-time contributor', async () => {
+    isSupportedEventSpy.mockReturnValue(true)
+    getInputSpy.mockReturnValue('***')
+    isFirstTimeContributorSpy.mockReturnValue(new Promise(resolve => resolve(false)))
+
+    await main.run()
+
+    expect(isFirstTimeContributorSpy).toHaveBeenCalled()
+    expect(getActionInputsSpyMock).not.toHaveBeenCalled()
+    expect(setOutputSpy).not.toHaveBeenCalled()
+    expect(runSpy).toHaveReturned()
+  })
 
   // Check for supported events:
   // 'issues.opened',
@@ -21,10 +58,4 @@ describe('first-contribution GitHub Action', () => {
   // 'pull_request_target.closed'
 
   // Check for correct outputs
-
-  it('runs action', async () => {
-    const runMock = jest.spyOn(main, 'run')
-    await main.run()
-    expect(runMock).toHaveReturned()
-  })
 })
