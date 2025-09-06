86af962d22d89bacf94bcc6bd757d8994b0d72f1
Paul Ebose <paulebose@gmail.com>
2025-09-06 08:45:51 +0100

    fix `runMock` in index test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -6,7 +6,7 @@ import { describe, expect, it, vitest } from 'vitest'
 import * as main from '../src/main.ts'
 
 // Mock the action's entrypoint
-const runMock = vitest.spyOn(main, 'run').mockReturnValue(Promise.resolve())
+const runMock = vitest.spyOn(main, 'run').mockReturnValue(Promise.resolve(false))
 
 describe('index', () => {
   it('calls run when imported', async () => {
