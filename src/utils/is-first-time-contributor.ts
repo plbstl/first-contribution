08119bb131be71815dd6ed6634eb0aaa5140a84d
Paul Ebose <paulebose@gmail.com>
2025-09-06 03:04:00 +0100

    fix check for when a user has their PR (merged/closed) or issue (completed/not-planned)
    If the author has any previously closed issue/PR in the repository, they are not a first timer

--- src/utils/is-first-time-contributor.ts
+++ src/utils/is-first-time-contributor.ts
@@ -1,35 +1,81 @@
-import type { Context } from '@actions/github/lib/context'
-import type { GitHub } from '@actions/github/lib/utils'
+import type { GitHub } from '@actions/github/lib/utils.ts'
 
 /**
- * Checks whether the author of the issue or pull request is a first-time contributor.
+ * Checks if an issue or PR author is a first-time contributor.
  *
- * If the action trigger is a `closed` event, this function takes into account
- * whether the author only has opened issues and PRs or not.
- * @param githubContext Context from the `@actions/github` library.
- * @param octokit - A GitHub Octokit client.
- * @returns `true` if author is a first-time contributor, and `false` otherwise.
+ * Users can have multiple opened issues and PRs, this action will have commented on only 1 of them.
+ * But the first time that they close an issue/PR, we have to decide if it is their first authored issue/PR.
+ *
+ * Issues and PRs have separate comments to be attached.
+ * A user can be a first-timer twice. Once for issues, and the other for PRs.
+ *
+ * `contribution-mode` action option can be set to tweak this behaviour.
+ *
+ * @param octokit A GitHub Octokit client.
+ * @param opts {@link IsFirstTimeContributorOpts}
+ * @returns `true` if the author meets the "first-time contributor" criteria
+ * for the given event, and `false` otherwise.
  */
 export async function isFirstTimeContributor(
-  githubContext: Context,
-  octokit: InstanceType<typeof GitHub>
+  octokit: InstanceType<typeof GitHub>,
+  opts: IsFirstTimeContributorOpts
 ): Promise<boolean> {
-  const payload = githubContext.payload
+  const { payload_action, ...listForRepoOpts } = opts
 
-  const response = await octokit.rest.issues.listForRepo({
-    ...githubContext.repo,
-    creator: payload.issue?.user.login,
-    state: 'all'
+  if (!payload_action) {
+    return false
+  }
+
+  const { data } = await octokit.rest.issues.listForRepo({
+    ...listForRepoOpts,
+    state: payload_action === 'opened' ? 'open' : 'closed'
   })
 
-  // Take into account whether the user only has opened issues and PRs,
-  // for when commenting on a closed issue or PR.
-  if (payload.action === 'closed') {
-    return !response.data.some(issueOrPullRequest => issueOrPullRequest.state === 'closed')
-  }
-
-  if (response.data.length === 1) {
+  // Only 1 issue/PR means the first contribution
+  if (data.length === 1) {
     return true
   }
-  return false
+
+  // Check if the issue/PR was authored by a first-timer.
+  // They can have multiple opened issues/PRs, this action will have commented on only 1 of them.
+
+  // But the first time that they close an issue/PR, we have to add a new comment (if any).
+
+  // Something to consider, is, issues and PRs have separate comments to be attached.
+  // In a way, a user can be a first-timer twice. Once for issues, and the other for PRs.
+
+  // This needs to be explicit in the docs.
+
+  let prCount = 0
+  let issueCount = 0
+
+  for (const doc of data) {
+    if (doc.pull_request) {
+      prCount++
+    } else {
+      issueCount++
+    }
+
+    // This also works for closed issues/PRs because if the count is only 1,
+    // that means the "1" is this current issue/PR.
+    if (issueCount > 1 || prCount > 1) {
+      return false
+    }
+  }
+
+  return true
+}
+
+interface IsFirstTimeContributorOpts {
+  /** Username of the user that created the issue or pull request. */
+  creator: string
+  /**
+   * The action that triggered the workflow run (e.g., 'opened', 'closed').
+   * Typically from `github.context.payload.action`.
+   */
+  payload_action?: string
+  /** Username of the repository's owner. */
+  owner: string
+  /** Name of the repository. */
+  repo: string
 }
