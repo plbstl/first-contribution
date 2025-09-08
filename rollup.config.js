c48094fbd0c026ff4266ba6af23bd9482514f1c4
Paul Ebose <paulebose@gmail.com>
2025-09-08 11:19:06 +0100

    support path aliasing in rollup, typescript & vitest
    [~src, ~tests]

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+import alias from '@rollup/plugin-alias'
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import swc from '@rollup/plugin-swc'
 import terser from '@rollup/plugin-terser'
+import path from 'node:path'
 
 const config = {
   input: 'src/index.ts',
@@ -11,7 +13,18 @@ const config = {
     format: 'es',
     sourcemap: true
   },
-  plugins: [commonjs(), nodeResolve(), swc(), terser()]
+  plugins: [
+    alias({
+      entries: [
+        { find: /^~src(.*)/, replacement: path.resolve(import.meta.dirname, 'src/$1') },
+        { find: /^~tests(.*)/, replacement: path.resolve(import.meta.dirname, 'tests/$1') }
+      ]
+    }),
+    commonjs(),
+    nodeResolve(),
+    swc(),
+    terser()
+  ]
 }
 
 export default config
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~src': path.resolve(import.meta.dirname, 'src'),
+      '~tests': path.resolve(import.meta.dirname, 'tests')
+    }
+  },
   test: {
     clearMocks: true,
     coverage: {
