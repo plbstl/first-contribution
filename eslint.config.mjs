b328de5251e68b11163f0bf0544fb8ab118d1b8d
Paul Ebose <pebose@proton.me>
2026-02-12 04:11:54 +0100

    fix eslint config
    Remove `eslint-plugin-github` for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+// @ts-check
+
 import js from '@eslint/js'
 import vitest from '@vitest/eslint-plugin'
-import github from 'eslint-plugin-github'
+// import github from 'eslint-plugin-github'
 import { importX } from 'eslint-plugin-import-x'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
@@ -10,14 +12,10 @@ export default defineConfig([
   globalIgnores(['node_modules', 'dist', 'coverage', 'eslint.config.mjs']),
 
   {
-    plugins: { github }
-  },
-
-  {
     extends: [
       js.configs.recommended,
-      tseslint.configs.strictTypeChecked,
-      tseslint.configs.stylisticTypeChecked,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
       importX.flatConfigs.recommended,
       importX.flatConfigs.typescript
     ],
@@ -33,13 +31,15 @@ export default defineConfig([
       }
     },
     rules: {
-      'github/array-foreach': 'error',
-      'github/filenames-match-regex': ['error', '^[a-z0-9-_]+(.[a-z0-9-_]+)?$'],
-      'github/no-then': 'error',
+      // 'github/array-foreach': 'error',
+      // 'github/filenames-match-regex': ['error', '^[a-z0-9-_]+(.[a-z0-9-_]+)?$'],
+      // 'github/no-then': 'error',
       'import-x/no-dynamic-require': 'error',
       'import-x/no-namespace': 'off',
       '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
       '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
+      '@typescript-eslint/no-deprecated': 'off',
+      '@typescript-eslint/consistent-generic-constructors': 'off',
       '@typescript-eslint/no-unnecessary-qualifier': 'error',
       '@typescript-eslint/no-var-requires': 'error',
       '@typescript-eslint/promise-function-async': 'error',
