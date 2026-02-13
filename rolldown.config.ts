d19a59d576949317438638f3568ce8985d555986
Paul Ebose <pebose@proton.me>
2026-02-13 01:56:07 +0100

    fix nodejs import error when in action runner

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,9 +1,9 @@
 import path from 'node:path'
 import { defineConfig } from 'rolldown'
-import { esmExternalRequirePlugin } from 'rolldown/plugins'
 
 export default defineConfig({
   input: 'src/index.ts',
+  platform: 'node',
   output: {
     minify: true,
     file: 'dist/index.js',
@@ -14,10 +14,5 @@ export default defineConfig({
       '~src': path.resolve(import.meta.dirname, 'src'),
       '~tests': path.resolve(import.meta.dirname, 'tests')
     }
-  },
-  plugins: [
-    esmExternalRequirePlugin({
-      external: ['@actions/core', '@actions/github']
-    })
-  ]
+  }
 })
