4854c8194206f3f73a4971d77a0a7f6ae929b239
Paul Ebose <pebose@proton.me>
2026-02-13 03:06:19 +0100

    fix JSDoc return type in main.run function

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ type ErrorOccurred = boolean
 
 /**
  * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
+ * @returns {Promise<ErrorOccurred>} Resolves when the action is complete.
  */
 export async function run(): Promise<ErrorOccurred> {
   try {
