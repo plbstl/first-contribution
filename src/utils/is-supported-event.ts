5b891866ae42b8141517498e888c16cd6a0d7349
Paul Ebose <pebose@proton.me>
2026-03-28 14:27:25 +0100

    trim is_supported_event jsdocs

--- src/utils/is-supported-event.ts
+++ src/utils/is-supported-event.ts
@@ -2,7 +2,6 @@
  * Checks whether the triggered event is supported by first-contribution GitHub Action.
  * @param event_name Name of the triggered event.
  * @param [action] Action that caused the event to trigger.
- * @returns
  */
 export function is_supported_event(event_name: string, action?: string): action is 'opened' | 'closed' {
   const event_code = `${event_name}.${String(action)}`
